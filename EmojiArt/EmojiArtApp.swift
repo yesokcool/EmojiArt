ce777d630f7993c8e3bf0c1b04f6d99704004559
yesokcool <ysokcl@gmail.com>
2022-06-14 14:45:29 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Matteo Galli on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
